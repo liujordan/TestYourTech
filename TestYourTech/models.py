4b5a34a852fd6ac3c6a68cb949304278e34a708b
Jordan Liu <liu.jordan.com@gmail.com>
2018-02-03 13:28:48 -0500

    added linkers for result and testcase

--- TestYourTech/models.py
+++ TestYourTech/models.py
@@ -9,15 +9,28 @@ class Action(models.Model):
 		Selector,
 		on_delete=models.CASCADE,
 		primary_key=True),
-	action_name = models.CharField(max_length)
-	results = models.ManyToMany
+	name = models.CharField(max_length=256)
+	testcases = models.ManyToManyField(
+		TestCase,
+		through='TestCaseLink',
+		through_fields=('action', 'testcase'))
+	results = models.ManyToManyField(
+		Result,
+		through='ResultLink',
+		through_fields=('action', 'result'))
 	action_link = models.ManyToManyField(
 		Action,
 		through='ActionLink',
-		through_fields=('action', 'next_action'),
-	)
+		through_fields=('action', 'next_action'))
+
 
 
 class ActionLink(models.Model):
 	action = models.ForeignKey(Action, on_delete=models.CASCADE)
 	next_action = models.ForeignKey(Action, on_delete=models.CASCADE)
+class ResultLink(models.Model):
+	action = models.ForeignKey(Action, on_delete=models.CASCADE)
+	result = models.ForeignKey(Result, on_delete=models.CASCADE)
+class TestCaseLink(models.Model):
+	action = models.ForeignKey(Action, on_delete=models.CASCADE)
+	testcase = models.ForeignKey(TestCase, on_delete=models.CASCADE)
