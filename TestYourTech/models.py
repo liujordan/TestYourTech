87fa9da80c86b2165388afa0be53b5a7d82acfe3
Jordan Liu <liu.jordan.com@gmail.com>
2018-02-03 13:23:20 -0500

    added actionlink model)

--- TestYourTech/models.py
+++ TestYourTech/models.py
@@ -3,10 +3,21 @@ ACTION_TYPES = [
 	('click', 'CLICK'),
 	('type', 'TYPE')]
 class Action(models.Model):
-	action_id = models.IntegerField()
-	action_type = models.CharField(max_length=256, choices=ACTION_TYPES, default='click')
+	id = models.IntegerField()
+	type = models.CharField(max_length=256, choices=ACTION_TYPES, default='click')
 	selector = models.OneToOneField(
 		Selector,
 		on_delete=models.CASCADE,
 		primary_key=True),
 	action_name = models.CharField(max_length)
+	results = models.ManyToMany
+	action_link = models.ManyToManyField(
+		Action,
+		through='ActionLink',
+		through_fields=('action', 'next_action'),
+	)
+
+
+class ActionLink(models.Model):
+	action = models.ForeignKey(Action, on_delete=models.CASCADE)
+	next_action = models.ForeignKey(Action, on_delete=models.CASCADE)
